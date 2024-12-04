eaf1ea816a5930558805bf0b548f3ed1cd8e2f72
zotekx <zote.kx@gmail.com>
2024-12-04 15:55:33 -0500

    deshabilitar valdiatePermission

--- apps/backend/src/middleware/validatePermission/validatePermission.ts
+++ apps/backend/src/middleware/validatePermission/validatePermission.ts
@@ -1,12 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import Boom from '@hapi/boom'
 import { NextFunction, Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
 
-import config from '../../config/config'
-import PermissionSingleton from '../singleton/permissionSigleton'
-
-const { JWTKey } = config
+// const { JWTKey } = config
 
 export const validatePermission = async (
   req: Request,
@@ -14,48 +10,49 @@ export const validatePermission = async (
   next: NextFunction,
 ): Promise<void> => {
   try {
-    const apiUrlPrefix = '/api/'
-    const currentUrl =
-      (`${req.path}`.split(apiUrlPrefix)[1] || req.path).replace(
-        /\/[^/]*$/,
-        '/?',
-      ) || req.originalUrl
-    const currentUrls = `${req.path}`.split(apiUrlPrefix)[1] || req.originalUrl
-    const _token = req.headers.authorization
-    console.log(currentUrls)
-    console.log(currentUrl)
-    if (!_token) {
-      next(Boom.unauthorized('No se envió el token'))
-    } else {
-      const token = _token.split(' ')[1]
-      if (!JWTKey) {
-        next(Boom.internal('El TOKEN NO FUE AÑADIDO AL .ENV'))
-      }
+    next()
+    // const apiUrlPrefix = '/api/'
+    // const currentUrl =
+    //   (`${req.path}`.split(apiUrlPrefix)[1] || req.path).replace(
+    //     /\/[^/]*$/,
+    //     '/?',
+    //   ) || req.originalUrl
+    // const currentUrls = `${req.path}`.split(apiUrlPrefix)[1] || req.originalUrl
+    // const _token = req.headers.authorization
+    // console.log(currentUrls)
+    // console.log(currentUrl)
+    // if (!_token) {
+    //   next(Boom.unauthorized('No se envió el token'))
+    // } else {
+    //   const token = _token.split(' ')[1]
+    //   if (!JWTKey) {
+    //     next(Boom.internal('El TOKEN NO FUE AÑADIDO AL .ENV'))
+    //   }
 
-      const decode: any = jwt.verify(token, JWTKey)
-      if (config.disabledPermissions) {
-        next()
-      } else if (decode) {
-        const rolId = decode.rol_id
-        const status = decode.status
-        const permissionSingleton = PermissionSingleton.getInstance()
-        if (permissionSingleton.rol != String(rolId)) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-          await permissionSingleton.fetchPermissionsFromDatabase(rolId)
-        }
-        if (
-          (permissionSingleton.hasPermission(currentUrl) ||
-            permissionSingleton.hasPermission(currentUrls)) &&
-          status
-        ) {
-          next()
-        } else {
-          next(Boom.unauthorized('Acceso no autorizado'))
-        }
-      } else {
-        next(Boom.internal('Error al decodificar el token'))
-      }
-    }
+    //   const decode: any = jwt.verify(token, JWTKey)
+    //   if (config.disabledPermissions) {
+    //     next()
+    //   } else if (decode) {
+    //     const rolId = decode.rol_id
+    //     const status = decode.status
+    //     const permissionSingleton = PermissionSingleton.getInstance()
+    //     if (permissionSingleton.rol != String(rolId)) {
+    //       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    //       await permissionSingleton.fetchPermissionsFromDatabase(rolId)
+    //     }
+    //     if (
+    //       (permissionSingleton.hasPermission(currentUrl) ||
+    //         permissionSingleton.hasPermission(currentUrls)) &&
+    //       status
+    //     ) {
+    //       next()
+    //     } else {
+    //       next(Boom.unauthorized('Acceso no autorizado'))
+    //     }
+    //   } else {
+    //     next(Boom.internal('Error al decodificar el token'))
+    //   }
+    // }
   } catch (error: any) {
     next(Boom.unauthorized(`Error al validar token: ${error.message}`))
   }
