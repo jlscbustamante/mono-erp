4b6d20c6c5d398665846a8567c6119f847732304
zotekx <zote.kx@gmail.com>
2024-12-04 17:34:51 -0500

    fix: requerimiento carga incial de recursos

--- apps/frontend/src/app/erp/modules/requerimientos/layout.tsx
+++ apps/frontend/src/app/erp/modules/requerimientos/layout.tsx
@@ -7,9 +7,18 @@ import Layout, {
 } from '@/components/layout'
 import { appConfig } from '@/const/config'
 import { PATHS } from '@/const/paths'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useOutlet } from 'react-router'
+import { useSetRecoilState } from 'recoil'
 import { useSession } from '../../use-session'
+
+// import * as sdk from '@/data/resources/sdk'
+import * as sdk from '@/data/requests/sdk'
+import {
+  cashAccountRequestSt,
+  categoriesRequestSt,
+  costCentersSt,
+} from '@/data/resources/state'
 
 const menu: (MenuOption | MenuSeparator)[] = [
   gm('Solicitados', PATHS.erp.modulos.requerimientos.solicitados),
@@ -27,6 +36,9 @@ const menu: (MenuOption | MenuSeparator)[] = [
 ]
 
 export const RequerimientosLayout = () => {
+  const setCategories = useSetRecoilState(categoriesRequestSt)
+  const setCashAccounts = useSetRecoilState(cashAccountRequestSt)
+  const setCostCenters = useSetRecoilState(costCentersSt)
   const views = useSession((st) => st.user.views)
   const outlet = useOutlet()
 
@@ -60,6 +72,18 @@ export const RequerimientosLayout = () => {
         .filter((el) => el) as (MenuOption | MenuSeparator)[],
     [views],
   )
+  useEffect(() => {
+    ;(async () => {
+      const [categories, cashAccounts, costCenters] = await Promise.all([
+        sdk.categories(),
+        sdk.cashAccount(),
+        sdk.costCenters(),
+      ])
+      setCategories(categories)
+      setCashAccounts(cashAccounts)
+      setCostCenters(costCenters)
+    })()
+  }, [])
 
   return (
     <Layout>
