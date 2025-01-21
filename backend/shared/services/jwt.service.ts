f77124b049bc31c9b55ae3d1c70cfee5779ca3f1
zotekx <zote.kx@gmail.com>
2025-01-21 14:40:15 -0500

    logs

--- backend/shared/services/jwt.service.ts
+++ backend/shared/services/jwt.service.ts
@@ -6,6 +6,7 @@ export class JwtService {
     secretKey: string,
     private readonly expiresIn = "30d"
   ) {
+    console.log("private : ", secretKey);
     this.secret = new TextEncoder().encode(secretKey);
   }
 
