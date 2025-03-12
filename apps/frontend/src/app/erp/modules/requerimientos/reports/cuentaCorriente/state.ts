24aaf384c2fbf71ce4cb3c4c85ef6c79d4a80a11
zotekx <zote.kx@gmail.com>
2025-03-12 17:27:42 -0500

    wip: new-requerimiento

--- apps/frontend/src/app/erp/modules/requerimientos/reports/cuentaCorriente/state.ts
+++ apps/frontend/src/app/erp/modules/requerimientos/reports/cuentaCorriente/state.ts
@@ -2,15 +2,15 @@ import { RequirementSelect, WhereOption } from '@pizzadb'
 import { format } from 'date-fns'
 import { create } from 'zustand'
 
-interface Item {
-  key: string | number
-  requestType: string
-  docNumber: string
-  requestedAt: string
-  legalName: string
-  subTitle: string
-  amount: number
-}
+// interface Item {
+//   key: string | number
+//   requestType: string
+//   docNumber: string
+//   requestedAt: string
+//   legalName: string
+//   subTitle: string
+//   amount: number
+// }
 
 interface IStore {
   refetch: () => void
