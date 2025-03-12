7060545189642e31c4ffee9b1c5d3ee678e2cbb6
zotekx <zote.kx@gmail.com>
2025-03-12 17:22:41 -0500

    wip: new-requerimiento

--- apps/frontend/src/app/erp/modules/requerimientos/review/page.tsx
+++ apps/frontend/src/app/erp/modules/requerimientos/review/page.tsx
@@ -8,6 +8,7 @@ export function RequirementReview() {
   const searchParams = new URLSearchParams(window.location.search)
   const id = searchParams.get('id') ? Number(searchParams.get('id')) : null
   const beforeUrl = searchParams.get('bf') ? searchParams.get('bf') : null
+
   const { data } = useQuery({
     queryKey: ['rq:requirement', id],
     enabled: !!id,
@@ -24,6 +25,7 @@ export function RequirementReview() {
       return data.data as IRequirementDetail
     },
   })
+
   return (
     <div className="p-3">
       {!data ? null : data.type == REQUIERMENT_TYPE.TRANSFER ? (
