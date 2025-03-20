37dfaffcdf58ee2b64593ace67585382a4568725
zotekx <zote.kx@gmail.com>
2025-03-20 11:01:18 -0500

    wip: new-requerimiento

--- apps/frontend/src/app/erp/modules/requerimientos/components/switch-view.tsx
+++ apps/frontend/src/app/erp/modules/requerimientos/components/switch-view.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/utils'
 import { Switch } from 'antd'
-import { Calendar, Logs } from 'lucide-react'
+import { Calendar, Logs, Table } from 'lucide-react'
 import { ViewType } from './view-type.interface'
 
 export function SwitchView({
@@ -10,6 +10,38 @@ export function SwitchView({
   view?: ViewType
   onChange?: (view: ViewType) => void
 }) {
+  return (
+    <div className="border border-slate-200 flex items-center bg-blue-50 p-0.5 rounded-md">
+      <div
+        onClick={() => {
+          onChange?.('list')
+        }}
+        className={cn('p-1.5 flex items-center justify-center', {
+          'bg-white shadow-md shadow-slate-400/10 cursor-pointer rounded-md text-slate-800 border border-solid border-slate-400/50':
+            view == 'calendar',
+          'text-slate-300 border-transparent': view == 'list',
+        })}
+      >
+        <Table className={cn('w-5 h-auto')} />
+      </div>
+      <div
+        onClick={() => {
+          onChange?.('calendar')
+        }}
+        className={cn(
+          'p-1.5 flex items-center justify-center border border-solid border-slate-400/50 rounded-md',
+          {
+            'bg-white shadow-md shadow-slate-400/10 cursor-pointer rounded-md text-slate-800':
+              view == 'list',
+            'text-slate-300 border-transparent': view == 'calendar',
+          },
+        )}
+      >
+        <Calendar className={cn('w-5 h-auto')} />
+      </div>
+    </div>
+  )
+
   return (
     <div className="flex rounded-md ml-2 gap-1">
       <Logs
