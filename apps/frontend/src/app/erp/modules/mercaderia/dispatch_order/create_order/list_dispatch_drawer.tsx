f8b147dd602b11915721eff33f2bfd0b9ab2c902
zotekx <zote.kx@gmail.com>
2025-04-30 16:03:19 -0500

    feat: refactor ListDispatchDrawer to improve item addition and input handling

--- apps/frontend/src/app/erp/modules/mercaderia/dispatch_order/create_order/list_dispatch_drawer.tsx
+++ apps/frontend/src/app/erp/modules/mercaderia/dispatch_order/create_order/list_dispatch_drawer.tsx
@@ -2,7 +2,7 @@ import { filterSelectForm } from '@/utils'
 import { IItem } from '@types'
 import { Button, Drawer, InputNumber, Select, Table } from 'antd'
 import { Trash } from 'lucide-react'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { toast } from 'react-toastify'
 
 export interface IItemToCreateDto extends IItem {
@@ -23,9 +23,12 @@ export const ListDispatchDrawer = ({
   items: IItemToCreateDto[]
   set_items: (items: IItemToCreateDto[]) => void
 }) => {
-  const [selected_item, set_selected_item] = useState<IItem | null>(null)
+  const table_ref = useRef<HTMLDivElement>(null)
+  const [selected_item, set_selected_item] = useState<IItem | undefined>(
+    undefined,
+  )
 
-  const handle_add_item = () => {
+  const _handle_add_item = () => {
     if (!selected_item) return
     const exist = items.find((item) => item.item_id == selected_item.item_id)
     if (exist) {
@@ -40,6 +43,22 @@ export const ListDispatchDrawer = ({
         },
       ])
     }
+  }
+
+  const add_item = (item: IItem) => {
+    const exist = items.find((i) => i.item_id == item.item_id)
+    if (exist) {
+      toast.error('El item ya fue agregado')
+      return
+    }
+    set_items([
+      ...items,
+      {
+        ...item,
+        quantity: 1,
+        total: item.store_price,
+      },
+    ])
   }
 
   const handle_remove_items = (item_id: number) => {
@@ -60,6 +79,19 @@ export const ListDispatchDrawer = ({
     })
     set_items(new_items)
   }
+
+  const next_input_number = (current_index: number) => {
+    if (!table_ref.current) return
+
+    const next_index = current_index + 1
+
+    const next_input = table_ref.current.querySelectorAll('input')[next_index]
+    if (next_input) {
+      next_input.focus()
+      ;(next_input as any).select()
+    }
+  }
+
   return (
     <Drawer
       title="Agrega items al despacho"
@@ -71,13 +103,16 @@ export const ListDispatchDrawer = ({
         <Select
           className="w-full"
           filterOption={filterSelectForm}
+          allowClear={true}
           showSearch
           value={selected_item?.item_id}
           onChange={(val) => {
             const item = dispatchable_items.find((item) => item.item_id == val)
             if (item) {
-              set_selected_item(item)
+              add_item(item)
             }
+            set_selected_item(undefined)
+            // set_selected_item(item)
           }}
         >
           {dispatchable_items?.map((item) => (
@@ -86,71 +121,78 @@ export const ListDispatchDrawer = ({
             </Select.Option>
           ))}
         </Select>
-        <Button type="primary" onClick={handle_add_item}>
+        {/* <Button type="primary" onClick={handle_add_item}>
           Agregar
-        </Button>
+        </Button> */}
       </div>
-      <Table
-        rowKey={'item_id'}
-        pagination={false}
-        size="small"
-        dataSource={items}
-        columns={[
-          {
-            title: 'Item id',
-            dataIndex: 'item_id',
-          },
-          {
-            title: 'Item',
-            dataIndex: 'item_name',
-          },
-          {
-            title: 'P.U',
-            dataIndex: 'store_price',
-          },
-          {
-            title: 'Cantidad',
-            render: (_, record) => {
-              return (
-                <InputNumber
-                  className="w-28"
-                  value={record.quantity}
-                  min={0}
-                  onChange={(val) => change_quantity(record.item_id, val ?? 0)}
-                />
-              )
+      <div ref={table_ref}>
+        <Table
+          rowKey={'item_id'}
+          pagination={false}
+          size="small"
+          dataSource={items}
+          columns={[
+            {
+              title: 'Item id',
+              dataIndex: 'item_id',
             },
-          },
-          {
-            title: 'Total',
-            dataIndex: 'total',
-            width: 120,
-            render: (val: number) => {
-              return val.toLocaleString('es-PE', {
-                style: 'currency',
-                currency: 'PEN',
-              })
+            {
+              title: 'Item',
+              dataIndex: 'item_name',
             },
-          },
-          {
-            title: '',
-            render: (_, record) => {
-              return (
-                <Button
-                  ghost
-                  size="small"
-                  className="text-blue-600 cursor-pointer"
-                  onClick={() => {
-                    handle_remove_items(record.item_id)
-                  }}
-                >
-                  <Trash className="w-4 text-blue-600" />
-                </Button>
-              )
+            {
+              title: 'P.U',
+              dataIndex: 'store_price',
             },
-          },
-        ]}
-      />
+            {
+              title: 'Cantidad',
+              render: (_, record, index) => {
+                return (
+                  <InputNumber
+                    className="w-28"
+                    value={record.quantity}
+                    min={0}
+                    onChange={(val) =>
+                      change_quantity(record.item_id, val ?? 0)
+                    }
+                    onPressEnter={() => {
+                      next_input_number(index)
+                    }}
+                  />
+                )
+              },
+            },
+            {
+              title: 'Total',
+              dataIndex: 'total',
+              width: 120,
+              render: (val: number) => {
+                return val.toLocaleString('es-PE', {
+                  style: 'currency',
+                  currency: 'PEN',
+                })
+              },
+            },
+            {
+              title: '',
+              render: (_, record) => {
+                return (
+                  <Button
+                    ghost
+                    size="small"
+                    className="text-blue-600 cursor-pointer"
+                    onClick={() => {
+                      handle_remove_items(record.item_id)
+                    }}
+                  >
+                    <Trash className="w-4 text-blue-600" />
+                  </Button>
+                )
+              },
+            },
+          ]}
+        />
+      </div>
     </Drawer>
   )
 }
